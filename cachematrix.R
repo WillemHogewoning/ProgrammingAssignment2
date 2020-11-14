ce4ca6a80eb29918f9ea70e3d1caee0c9ea02df3
WillemHogewoning <Willem@hogewoning.me>
2020-11-14 19:52:05 +0100

    Testing the see if the Fork, Clone and commit is working.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,11 @@
+# 1): Fork the GitHub repository containing the stub R files at https://github.com/rdpeng/ProgrammingAssignment2
+# 2): Clone your forked GitHub repository to your computer so that you can edit the files locally on your own machine.
+# 3): Edit the R file contained in the git repository and place your solution in that file (please do not rename the file)
+# 4): Commit your completed R file into YOUR git repository and push your git branch to the GitHub repository under your account.
+
+# Started witht the Fork, Clone, and Test commit.
+
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
